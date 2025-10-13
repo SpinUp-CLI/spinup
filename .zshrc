0f0f42e7e74c2078ca62c1326e89a35a437e1071
elouannh <elouannhosta27@gmail.com>
2025-10-13 17:53:24 +0200

    Default vue template for testing

---
+++ .zshrc
@@ -0,0 +1,2 @@
+export GOPATH="/home/ehosta/go"
+export GOROOT="/home/ehosta/local/go/go"
---
+++ cmd/new.go
@@ -0,0 +1,22 @@
+
+/*
+Copyright © 2025 Elouann Hosta ehosta@student.42lyon.fr
+*/
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+)
+
+var newCmd = &cobra.Command{
+	Use:   "new",
+	Short: "Soon.",
+	Long:  `Soon.`,
+	Run: func(cmd *cobra.Command, args []string) {
+
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(newCmd)
+}
